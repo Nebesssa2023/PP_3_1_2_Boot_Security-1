2b54ed9c6ed3347073c6b1662ca6eb15147cc7f5
Gabbasov Victor <victtss@gmail.com>
2023-01-10 16:07:07 +0300

    WS12"UserController+"

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
@@ -2,16 +2,9 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.jetbrains.annotations.Contract;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.Author;
-import ru.kata.spring.boot_security.demo.entity.User;
-import ru.kata.spring.boot_security.demo.service.UserService;
 
 
 @Author(name = "Victor Gabbasov", dateOfCreation = 2023)
@@ -20,13 +13,6 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 @RequestMapping("/")
 public class LoginController {
 
-    UserService userService;
-
-    @Contract(pure = true)
-    @Autowired
-    public LoginController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/login")
     public String login() {
@@ -36,18 +22,5 @@ public class LoginController {
     @GetMapping("/")
     public String home() {
         return "home";
-    }
-
-    @GetMapping("/user")
-    public String showUser(Model model) {
-        User user = userService.findByUserName(getCurrentUsername());
-        model.addAttribute("roles", user.getRoles());
-        model.addAttribute("user", user);
-        return "user";
-    }
-
-    public String getCurrentUsername() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return auth.getName();
     }
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,40 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.entity.Author;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Author(name = "Victor Gabbasov", dateOfCreation = 2023)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String showUser(Model model) {
+        User user = userService.findByUserName(getCurrentUsername());
+        model.addAttribute("roles", user.getRoles());
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    public String getCurrentUsername() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
+}
